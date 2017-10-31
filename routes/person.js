12ca71371025045523a43ecc43704ca919fdd2b7
Mario Jorquera <mariojz@gmail.com>
2017-10-31 14:54:15 -0300

    Initial commit with Express

---
+++ routes/person.js
@@ -0,0 +1,22 @@
+var express = require('express');
+
+var bodyParser = require('body-parser');
+var urlencode = bodyParser.urlencoded({ extended: false});
+
+// var redis = require('redis');
+// if (process.env.REDISTOGO_URL) {
+//     var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+//     var client = redis.createClient(rtg.port, rtg.hostname);
+//     client.auth(rtg.auth.split(":")[1]);
+// } else {
+//     var client = redis.createClient();
+//     client.select((process.env.NODE_ENV || 'development').length);
+// }
+
+var router = express.Router();
+
+router.route('/')
+    .get(function(request, response){
+    });
+
+    module.exports = router;
